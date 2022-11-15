ff2fc3e7ead8f66c6ffe5e27fa432f7d487f271a
Adun <>
2022-11-15 19:59:56 +0800

    kotlin

--- src/tasks/Request4Suspend.kt
+++ src/tasks/Request4Suspend.kt
@@ -3,5 +3,19 @@ package tasks
 import contributors.*
 
 suspend fun loadContributorsSuspend(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    var repos = service
+        .getOrgRepos(req.org)
+//        .execute() // Executes request and blocks the current thread
+        .also { logRepos(req, it) }
+        .body() ?: emptyList<Repo>()
+        // why this filter does not work???
+        .filter { it.name.contains("stack") }
+    repos = repos.filter { it.name.contains("stack") }
+    return repos.flatMap { repo ->
+        service
+            .getRepoContributors(req.org, repo.name)
+//            .execute() // Executes request and blocks the current thread
+            .also { logUsers(repo, it) }
+            .bodyList()
+    }.aggregate()
 }
